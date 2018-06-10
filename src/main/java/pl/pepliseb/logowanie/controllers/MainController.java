64d3ef7afc4002c6cfd7d5020d019b65a01f3c32
pepliseb <eman@onet.eu>
2018-06-10 23:18:31 +0200

    th add

--- src/main/java/pl/pepliseb/logowanie/controllers/MainController.java
+++ src/main/java/pl/pepliseb/logowanie/controllers/MainController.java
@@ -1,8 +1,15 @@
 package pl.pepliseb.logowanie.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
+    @GetMapping("/login")
+    @ResponseBody
+    public String index() {
+    return "loginTemplate";
+}
 }
